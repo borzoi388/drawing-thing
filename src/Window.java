24725cc1dfc6b8a13aa18df5aaeeca49d1b51a34
Hanya You <youh@catlin.edu>
2026-03-23 14:16:46 -0700

    feat: changing canvas size and bg color works

--- src/Window.java
+++ src/Window.java
@@ -1,19 +1,31 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
 public class Window {
+    boolean showGrid = true;
+
     private JFrame window;
     private Canvas canvas;
-    private int defaultWidth = 100;
-    private int defaultHeight = 100;
+    private int defaultWidth = 10;
+    private int defaultHeight = 10;
+    private int width;
+    private int height;
+
+
+    JPanel panel = new JPanel();
+    CanvasPanel canvasPanel = new CanvasPanel();
+    ControlPanel controlPanel = new ControlPanel();
 
     Window(int h, int w) {
         window = new JFrame();
         canvas = new Canvas(defaultHeight, defaultWidth, new Color(255, 255, 255));
-        JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
-        panel.add(new CanvasPanel(), BorderLayout.CENTER);
-        panel.add(new ControlPanel(), BorderLayout.SOUTH);
+        panel.add(canvasPanel, BorderLayout.CENTER);
+        panel.add(controlPanel, BorderLayout.NORTH);
 
         window.getContentPane().add(panel);
         window.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
@@ -28,12 +40,11 @@ public class Window {
         int offsetX;
         int offsetY;
         CanvasPanel() {
-            System.out.println(getSize().width);
-            setSize(800, 600);
-            calculatePixelSize();
+            addComponentListener(new ResizeListener());
         }
 
         void calculatePixelSize() {
+            if (canvas == null) {return;}
             Dimension size = getSize();
             pixelSize = (canvas.getHeight()/canvas.getWidth() > size.height/size.width) ? size.height/canvas.getHeight() : size.width/canvas.getWidth();
             offsetX = (size.width-pixelSize*canvas.getWidth())/2;
@@ -41,7 +52,7 @@ public class Window {
         }
 
         protected void paintComponent(Graphics pen) {
-            pen.setColor(new Color(255, 0, 0));
+            pen.setColor(new Color(100, 100, 100));
             pen.fillRect(0, 0, getSize().width, getSize().height);
             for (int r = 0; r < canvas.getHeight(); r++) {
                 for (int c = 0; c < canvas.getWidth(); c++) {
@@ -56,23 +67,70 @@ public class Window {
         private void drawPixel(Graphics pen, Pixel pixel) {
             int col = pixel.getCol()*pixelSize+offsetX;
             int row = pixel.getRow()*pixelSize+offsetY;
-            pen.setColor(new Color(0, 0, 0));
-            pen.drawRect(col, row, pixelSize, pixelSize);
-            pen.setColor(pixel.getColor());
-            pen.fillRect(col+1, row+1, pixelSize-2, pixelSize-2);
+            if (showGrid) {
+                pen.setColor(pixel.getColor());
+                pen.fillRect(col + 1, row + 1, pixelSize - 2, pixelSize - 2);
+            } else {
+                pen.setColor(pixel.getColor());
+                pen.fillRect(col, row, pixelSize, pixelSize);
+            }
+        }
+
+        private class ResizeListener implements ComponentListener {
+
+            @Override
+            public void componentResized(ComponentEvent e) {
+                calculatePixelSize();
+                repaint();
+            }
+
+            public void componentMoved(ComponentEvent e) {}
+
+            @Override
+            public void componentShown(ComponentEvent e) {}
+
+            @Override
+            public void componentHidden(ComponentEvent e) {}
         }
     }
 
     private class ControlPanel extends JPanel {
-        JTextField width, height;
+        JTextField widthTextField, heightTextField;
+        JButton newCanvasBtn;
+        JColorChooser colorChooser;
         ControlPanel() {
-            width = new JTextField(defaultWidth+"", 4);
-            height = new JTextField(defaultHeight+"", 4);
+            widthTextField = new JTextField(defaultWidth+"", 4);
+            heightTextField = new JTextField(defaultHeight+"", 4);
+            colorChooser = new JColorChooser(Color.white);
+            colorChooser.
+
+            newCanvasBtn = new JButton("New Canvas");
+            newCanvasBtn.addActionListener(new NewPaintingListener());
             setLayout(new FlowLayout());
             add(new JLabel("Width: "));
-            add(width);
+            add(widthTextField);
             add(new JLabel("Height: "));
-            add(height);
+            add(heightTextField);
+            add(new JLabel("Background color: "));
+            add(colorChooser);
+            add(newCanvasBtn);
+        }
+
+        private class NewPaintingListener implements ActionListener {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    width = Integer.parseInt(widthTextField.getText());
+                    height = Integer.parseInt(heightTextField.getText());
+                    canvas = new Canvas(width, height, colorChooser.getColor());
+                    canvasPanel.calculatePixelSize();
+                    canvasPanel.repaint();
+                } catch (Exception ex) {
+                    System.out.println("Fialure");
+                }
+            }
         }
     }
+
 }
